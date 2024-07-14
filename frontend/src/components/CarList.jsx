4c27ecf5004ff0abe3d97b95a44fd16d096c6e30
hamza <hamza.maksumic@protonmail.com>
2024-07-14 12:20:47 +0200

    feat: add simple and regular view options

--- frontend/src/components/CarList.jsx
+++ frontend/src/components/CarList.jsx
@@ -1,17 +1,19 @@
 import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import './CarList.css';
+import './PersonalCarList.css';
 import { Link } from 'react-router-dom';
 import AuthContext from '../context/AuthContext';
 
-const CarList = ({url, audi, bmw, mercedes, peugeot, volvo, volkswagen, other}) => {
-  const { user, favorites, addFavorite, removeFavorite, logout} = useContext(AuthContext);
+const CarList = ({ url, audi, bmw, mercedes, peugeot, volvo, volkswagen, other }) => {
+  const { user, favorites, addFavorite, removeFavorite, logout } = useContext(AuthContext);
   const [carData, setCarData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [searchTerm, setSearchTerm] = useState('');
   const [showAllPrices, setShowAllPrices] = useState({});
   const [sortBy, setSortBy] = useState('Calculated profit rate');
+  const [viewMode, setViewMode] = useState('regular');
 
   useEffect(() => {
     axios.get(url)
@@ -23,7 +25,7 @@ const CarList = ({url, audi, bmw, mercedes, peugeot, volvo, volkswagen, other}) 
         setError(error);
         setLoading(false);
       });
-  }, []);
+  }, [url]);
 
   if (loading) return <p>This should not take more than 50 seconds...</p>;
   if (error) return <p>Error loading data: {error.message}</p>;
@@ -32,7 +34,7 @@ const CarList = ({url, audi, bmw, mercedes, peugeot, volvo, volkswagen, other}) 
     return (parameter / 5.85).toFixed(0);
   }
 
-  const CalculateProfitRate = (olxPrices, finnPriceNOK, taxReturn=0) => {
+  const CalculateProfitRate = (olxPrices, finnPriceNOK, taxReturn = 0) => {
     const averageOLXPrice = olxPrices.reduce((sum, price) => sum + price, 0) / olxPrices.length;
     const finnPriceBAM = TurnToBAM(finnPriceNOK) - TurnToBAM(taxReturn);
     return finnPriceBAM / averageOLXPrice;
@@ -52,7 +54,7 @@ const CarList = ({url, audi, bmw, mercedes, peugeot, volvo, volkswagen, other}) 
         case 'Most matches':
           return b.olx_prices.length - a.olx_prices.length;
         case 'Highest tax-return':
-          return b.tax_return - a.tax_return;  
+          return b.tax_return - a.tax_return;
         case 'Calculated profit rate':
         default:
           return CalculateProfitRate(a.olx_prices, a.finn_price, a.tax_return) - CalculateProfitRate(b.olx_prices, b.finn_price, b.tax_return);
@@ -111,7 +113,7 @@ const CarList = ({url, audi, bmw, mercedes, peugeot, volvo, volkswagen, other}) 
     }
   };
   return (
-    <div className="car-list">
+    <div className={viewMode === 'regular' ? "car-list" : "car-list-personal"}>
       <div className="buttonbar">
         <div className="button">
           <Link to="/volkswagen" style={{ textDecoration: 'none' }}>
@@ -173,13 +175,16 @@ const CarList = ({url, audi, bmw, mercedes, peugeot, volvo, volkswagen, other}) 
         </div>
       </div>
       <div className="sort-bar-container">
-          <div className="sort-bar">
-        <label style={{marginRight:'20px', fontSize:'17px'}}>Sort by:</label>
-        <button className={`sort-button ${sortBy === 'Calculated profit rate' ? 'selected' : ''}`} onClick={() => setSortBy('Calculated profit rate')}>Profit potential</button>
-        <button className={`sort-button ${sortBy === 'Highest tax-return' ? 'selected' : ''}`} onClick={() => setSortBy('Highest tax-return')}>Highest tax-return</button>
-        <button className={`sort-button ${sortBy === 'Newest first' ? 'selected' : ''}`} onClick={() => setSortBy('Newest first')}>Newest first</button>
-        <button className={`sort-button ${sortBy === 'Most matches' ? 'selected' : ''}`} onClick={() => setSortBy('Most matches')}>Most matches</button>
-      </div>
+        <div className="sort-bar">
+          <label style={{ marginRight: '20px', fontSize: '17px' }}>Sort by:</label>
+          <button className={`sort-button ${sortBy === 'Calculated profit rate' ? 'selected' : ''}`} onClick={() => setSortBy('Calculated profit rate')}>Profit potential</button>
+          <button className={`sort-button ${sortBy === 'Highest tax-return' ? 'selected' : ''}`} onClick={() => setSortBy('Highest tax-return')}>Highest tax-return</button>
+          <button className={`sort-button ${sortBy === 'Newest first' ? 'selected' : ''}`} onClick={() => setSortBy('Newest first')}>Newest first</button>
+          <button className={`sort-button ${sortBy === 'Most matches' ? 'selected' : ''}`} onClick={() => setSortBy('Most matches')}>Most matches</button>
+          <button className="favorite-button" onClick={() => setViewMode(viewMode === 'regular' ? 'simple' : 'regular')} style={{ padding: '10px 20px', fontSize: '16px', cursor: 'pointer' }}>
+            {viewMode === 'regular' ? 'Switch to Simple View' : 'Switch to Regular View'}
+          </button>
+        </div>
       </div>
       <div className="search-bar-container">
         <input
@@ -192,44 +197,57 @@ const CarList = ({url, audi, bmw, mercedes, peugeot, volvo, volkswagen, other}) 
         <div className="result-count">Results: {sortedCarData.length}</div>
       </div>
       {sortedCarData.map((car, index) => (
-        <div key={index} className="car-card">
-          <div className="car-name-container">
-            <h2 className="car-name">{car.car_name}</h2>
-          </div>
-          <img src={car.image_url} alt={car.car_name} className="car-image" />
-          <p><strong>Finn.no link:</strong> <a href={car.finn_link} target="_blank" rel="noopener noreferrer">{car.finn_link}</a></p>
-          <p><strong>Finn.no price:</strong> {car.finn_price} NOK / {TurnToBAM(car.finn_price)} BAM </p>
-          <p><strong>OLX.ba prices:</strong> {
-            car.olx_prices
-              .map((price, i) => ({ price, url: `${BaseOLXUrl}${car.olx_ids[i]}` }))
-              .sort((a, b) => b.price - a.price)
-              .slice(0, showAllPrices[car.car_name] ? car.olx_prices.length : 5)
-              .map((item, i, arr) => (
-                <span key={i}>
-                  <a href={item.url} target="_blank" rel="noopener noreferrer" className="olx-link">
-                    {item.price === 0 ? 'Na upit' : item.price}
-                  </a>
-                  {i < arr.length - 1 && ', '}
-                </span>
-              ))
-          }
-            {car.olx_prices.length > 5 && (
-              <button onClick={() => toggleShowPrices(car.car_name)} className="more-button">
-                {showAllPrices[car.car_name] ? 'Less' : 'Show more...'}
-              </button>
-            )}
-          </p>
-
-          <p><strong>Year:</strong> {car.year}</p>
-          {car.tax_return > 0 && (
-            <p><strong>Norwegian tax return estimate:</strong> {car.tax_return} NOK / {TurnToBAM(car.tax_return)} BAM</p>
-          )}
-          {user && (
-            <div className="favorite-button-container" onClick={() => handleFavoriteClick(car)}>
-              <button className="favorite-button"> Save </button>
+        viewMode === 'regular' ? (
+          <div key={index} className="car-card">
+            <div className="car-name-container">
+              <h2 className="car-name">{car.car_name}</h2>
             </div>
-          )}
-        </div>
+            <img src={car.image_url} alt={car.car_name} className="car-image" />
+            <p><strong>Finn.no price:</strong> <a href={car.finn_link} target="_blank" rel="noopener noreferrer">{car.finn_price} NOK / {TurnToBAM(car.finn_price)} BAM </a></p>
+            <p><strong>OLX.ba prices:</strong> {
+              car.olx_prices
+                .map((price, i) => ({ price, url: `${BaseOLXUrl}${car.olx_ids[i]}` }))
+                .sort((a, b) => b.price - a.price)
+                .slice(0, showAllPrices[car.car_name] ? car.olx_prices.length : 5)
+                .map((item, i, arr) => (
+                  <span key={i}>
+                    <a href={item.url} target="_blank" rel="noopener noreferrer" className="olx-link">
+                      {item.price === 0 ? 'Na upit' : item.price}
+                    </a>
+                    {i < arr.length - 1 && ', '}
+                  </span>
+                ))
+            }
+              {car.olx_prices.length > 5 && (
+                <button onClick={() => toggleShowPrices(car.car_name)} className="more-button">
+                  {showAllPrices[car.car_name] ? 'Less' : 'Show more...'}
+                </button>
+              )}
+            </p>
+            <p><strong>Year:</strong> {car.year}</p>
+            {car.tax_return > 0 && (
+              <p><strong>Norwegian tax return estimate:</strong> {car.tax_return} NOK / {TurnToBAM(car.tax_return)} BAM</p>
+            )}
+            {user && (
+              <div className="favorite-button-container" onClick={() => handleFavoriteClick(car)}>
+                <button className="favorite-button"> Save </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <a key={index} href={car.finn_link} target="_blank" rel="noopener noreferrer" className="car-bar-personal">
+            <img src={car.image_url} alt={car.car_name} />
+            <div className="car-details-personal">
+              <p><strong>{car.car_name}</strong></p>
+              <p>Finn.no price: {car.finn_price} NOK / {TurnToBAM(car.finn_price)} BAM</p>
+              <p>Average OLX.ba price: {Math.round(car.olx_prices.reduce((a, b) => a + b, 0) / car.olx_prices.length)} BAM</p>
+              <p>Year: {car.year}</p>
+              {car.tax_return > 0 && (
+                <p>Tax return: {car.tax_return} NOK / {TurnToBAM(car.tax_return)} BAM</p>
+              )}
+            </div>
+          </a>
+        )
       ))}
     </div>
   );
--- frontend/src/components/PersonalCarList.jsx
+++ frontend/src/components/PersonalCarList.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useContext } from 'react';
 import axios from 'axios';
-import './CarList.css';
+import './PersonalCarList.css';
 import { Link } from 'react-router-dom';
 import AuthContext from '../context/AuthContext';
 
@@ -12,6 +12,8 @@ const PersonalCarList = () => {
   const [error, setError] = useState(null);
   const [searchTerm, setSearchTerm] = useState('');
   const [showAllPrices, setShowAllPrices] = useState({});
+  const [sortBy, setSortBy] = useState('Calculated profit rate');
+  const [viewMode, setViewMode] = useState('regular');
 
   useEffect(() => {
     const fetchFavorites = async () => {
@@ -23,10 +25,10 @@ const PersonalCarList = () => {
         });
         if (response.data.success) {
           const fetchedFavorites = response.data.favorites;
-          fetchedFavorites.reverse(); //for displaying the newly favorited cars first
+          fetchedFavorites.reverse(); // for displaying the newly favorited cars first
 
-          setFavorites(response.data.favorites);
-          setCarData(response.data.favorites);
+          setFavorites(fetchedFavorites);
+          setCarData(fetchedFavorites);
         } else {
           console.error(response.data.message);
         }
@@ -61,11 +63,9 @@ const PersonalCarList = () => {
     }
   }
 
-
   function TurnToBAM(parameter) {
     return (parameter / 5.85).toFixed(0);
   }
-
 
   const toggleShowPrices = (id) => {
     setShowAllPrices(prev => ({ ...prev, [id]: !prev[id] }));
@@ -97,65 +97,70 @@ const PersonalCarList = () => {
   const filteredCars = carData.filter(car => car.car_name.toLowerCase().includes(searchTerm));
 
   return (
-    <div className="car-list">
-    <div className="buttonbar">
+    <div className={viewMode === 'regular' ? "car-list" : "car-list-personal"}>
+      <div className="buttonbar">
         <div className="button">
           <Link to="/volkswagen" style={{ textDecoration: 'none' }}>
             <button style={{ padding: '10px 20px', fontSize: '16px', cursor: 'pointer', position: 'sticky' }}>
               Volkswagen
             </button>
           </Link>
-
           <Link to="/audi" style={{ textDecoration: 'none' }}>
             <button style={{ padding: '10px 20px', fontSize: '16px', cursor: 'pointer', position: 'sticky' }}>
               Audi
             </button>
           </Link>
-
           <Link to="/bmw" style={{ textDecoration: 'none' }}>
             <button style={{ padding: '10px 20px', fontSize: '16px', cursor: 'pointer', position: 'sticky' }}>
               BMW
             </button>
           </Link>
-
           <Link to="/mercedes" style={{ textDecoration: 'none' }}>
             <button style={{ padding: '10px 20px', fontSize: '16px', cursor: 'pointer', position: 'sticky' }}>
               Mercedes-Benz
             </button>
           </Link>
-
           <Link to="/peugeot" style={{ textDecoration: 'none' }}>
             <button style={{ padding: '10px 20px', fontSize: '16px', cursor: 'pointer', position: 'sticky' }}>
               Peugeot
             </button>
           </Link>
-
           <Link to="/volvo" style={{ textDecoration: 'none' }}>
             <button style={{ padding: '10px 20px', fontSize: '16px', cursor: 'pointer', position: 'sticky' }}>
               Volvo
             </button>
           </Link>
-
           <Link to="/other" style={{ textDecoration: 'none' }}>
             <button style={{ padding: '10px 20px', fontSize: '16px', cursor: 'pointer', position: 'sticky' }}>
               Other
             </button>
           </Link>
-
           {user && (
             <>
               <Link to="/personal" style={{ textDecoration: 'none' }}>
-                <button style={{ padding: '10px 20px', fontSize: '16px', cursor: 'pointer', position: 'sticky' , fontWeight: 'bold'}}>
+                <button style={{ padding: '10px 20px', fontSize: '16px', cursor: 'pointer', position: 'sticky', fontWeight: 'bold' }}>
                   Personal
                 </button>
               </Link>
               <Link to="/home">
-              <button onClick={logout} style={{ padding: '10px 20px', fontSize: '16px', cursor: 'pointer', position: 'sticky', backgroundColor: 'lightsteelblue' }}>
-                Logout
-              </button>
+                <button onClick={logout} style={{ padding: '10px 20px', fontSize: '16px', cursor: 'pointer', position: 'sticky', backgroundColor: 'lightsteelblue' }}>
+                  Logout
+                </button>
               </Link>
             </>
           )}
+        </div>
+      </div>
+      <div className="sort-bar-container">
+        <div className="sort-bar">
+          <label style={{ marginRight: '20px', fontSize: '17px' }}>Sort by:</label>
+          <button className={`sort-button ${sortBy === 'Calculated profit rate' ? 'selected' : ''}`} onClick={() => setSortBy('Calculated profit rate')}>Profit potential</button>
+          <button className={`sort-button ${sortBy === 'Highest tax-return' ? 'selected' : ''}`} onClick={() => setSortBy('Highest tax-return')}>Highest tax-return</button>
+          <button className={`sort-button ${sortBy === 'Newest first' ? 'selected' : ''}`} onClick={() => setSortBy('Newest first')}>Newest first</button>
+          <button className={`sort-button ${sortBy === 'Most matches' ? 'selected' : ''}`} onClick={() => setSortBy('Most matches')}>Most matches</button>
+          <button className="favorite-button" onClick={() => setViewMode(viewMode === 'regular' ? 'simple' : 'regular')} style={{ padding: '10px 20px', fontSize: '16px', cursor: 'pointer' }}>
+            {viewMode === 'regular' ? 'Switch to Simple View' : 'Switch to Regular View'}
+          </button>
         </div>
       </div>
       <div className="search-bar-container">
@@ -169,48 +174,61 @@ const PersonalCarList = () => {
         <div className="result-count">Results: {filteredCars.length}</div>
       </div>
       {filteredCars.map((car, index) => (
-        <div key={index} className="car-card">
-          <div className="car-name-container">
-          <h2 className="car-name">{car.car_name}</h2>
-          </div>
-          <img src={car.image_url} alt={car.car_name} className="car-image" />
-          <p><strong>Finn.no link:</strong> <a href={car.finn_link} target="_blank" rel="noopener noreferrer">{car.finn_link}</a></p>
-          <p><strong>Finn.no price:</strong> {car.finn_price} NOK / {TurnToBAM(car.finn_price)} BAM</p>
-          <p><strong>OLX.ba prices:</strong> {
-            car.olx_prices
-              .map((price, i) => ({ price, url: `${BaseOLXUrl}${car.olx_ids[i]}` }))
-              .sort((a, b) => b.price - a.price)
-              .slice(0, showAllPrices[car.car_name] ? car.olx_prices.length : 5)
-              .map((item, i, arr) => (
-                <span key={i}>
-                  <a href={item.url} target="_blank" rel="noopener noreferrer" className="olx-link">
-                    {item.price === 0 ? 'Na upit' : item.price}
-                  </a>
-                  {i < arr.length - 1 && ', '}
-                </span>
-              ))
-          }
-            {car.olx_prices.length > 5 && (
-              <button onClick={() => toggleShowPrices(car.car_name)} className="more-button">
-                {showAllPrices[car.car_name] ? 'Less' : 'Show more...'}
-              </button>
+        viewMode === 'regular' ? (
+          <div key={index} className="car-card">
+            <div className="car-name-container">
+              <h2 className="car-name">{car.car_name}</h2>
+            </div>
+            <img src={car.image_url} alt={car.car_name} className="car-image" />
+            <p><strong>Finn.no price:</strong> <a href={car.finn_link} target="_blank" rel="noopener noreferrer">{car.finn_price} NOK / {TurnToBAM(car.finn_price)} BAM</a></p>
+            <p><strong>OLX.ba prices:</strong> {
+              car.olx_prices
+                .map((price, i) => ({ price, url: `${BaseOLXUrl}${car.olx_ids[i]}` }))
+                .sort((a, b) => b.price - a.price)
+                .slice(0, showAllPrices[car.car_name] ? car.olx_prices.length : 5)
+                .map((item, i, arr) => (
+                  <span key={i}>
+                    <a href={item.url} target="_blank" rel="noopener noreferrer" className="olx-link">
+                      {item.price === 0 ? 'Na upit' : item.price}
+                    </a>
+                    {i < arr.length - 1 && ', '}
+                  </span>
+                ))
+            }
+              {car.olx_prices.length > 5 && (
+                <button onClick={() => toggleShowPrices(car.car_name)} className="more-button">
+                  {showAllPrices[car.car_name] ? 'Less' : 'Show more...'}
+                </button>
+              )}
+            </p>
+            <p><strong>Year:</strong> {car.year}</p>
+            {car.tax_return > 0 && (
+              <p><strong>Norwegian tax return estimate:</strong> {car.tax_return} NOK / {TurnToBAM(car.tax_return)} BAM</p>
             )}
-          </p>
-          <p><strong>Year:</strong> {car.year}</p>
-          {car.tax_return > 0 && (
-            <p><strong>Norwegian tax return estimate:</strong> {car.tax_return} NOK / {TurnToBAM(car.tax_return)} BAM</p>
-          )}
-          {user && (
-            <button onClick={() => handleRemoveFavorite(car.id)} className="favorite-button">
-              Remove
-            </button>
-          )}
-        </div>
+            {user && (
+              <div className="favorite-button-container" onClick={() => handleRemoveFavorite(car.id)}>
+                <button className="favorite-button">Remove</button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <a key={index} href={car.finn_link} target="_blank" rel="noopener noreferrer" className="car-bar-personal">
+            <img src={car.image_url} alt={car.car_name} />
+            <div className="car-details-personal">
+              <p><strong>{car.car_name}</strong></p>
+              <p>Finn.no price: {car.finn_price} NOK / {TurnToBAM(car.finn_price)} BAM</p>
+              <p>Average OLX.ba price: {Math.round(car.olx_prices.reduce((a, b) => a + b, 0) / car.olx_prices.length)} BAM</p>
+              <p>Year: {car.year}</p>
+              {car.tax_return > 0 && (
+                <p>Tax return: {car.tax_return} NOK / {TurnToBAM(car.tax_return)} BAM</p>
+              )}
+            </div>
+          </a>
+        )
       ))}
-      {filteredCars.length == 0 && (
+      {filteredCars.length === 0 && (
         <p>All saved cars will be displayed here</p>
-      )
-      }
+      )}
     </div>
   );
 };
